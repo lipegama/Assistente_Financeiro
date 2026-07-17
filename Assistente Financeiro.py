946a91064473a6189e0b892cee63bb693a882f81
Felipe Gama <150298976+lipegama@users.noreply.github.com>
2026-07-17 16:07:53 -0400

    Add files via upload
    Aqui está o código

---
+++ Assistente Financeiro.py
@@ -0,0 +1,34 @@
+#Crie um assistente financeiro que converse com seu cliente e oriente. # 
+
+
+transacoes = ['luz', 'água', 'mercado', 'lazer']
+preco = [350,100,250,500]
+data = ['03/06/26', '25/04/26', '26/06/26', '31/04/06']
+
+#menu de opção  
+while True:
+    print('Olá eu sou o primeiro Assistente Financeiro')
+    print('\n' + '='*30)
+    print('1- Visualizar o estrato da conta')
+    print('2- Adicionar nova Transação')
+    print('3- ver Total de Gastos')
+    print('q sair do sistema')
+    print('\n'+'='*30)
+    opcao=input('Escolha uma opção ').strip().lower()
+    if opcao == 'q':
+          print('\n Saindo do sistema')
+          break
+    elif opcao=='1':
+         print('\n =1- Visualizar o extrato atual')   
+         for i, item, valor, dt in zip(range(len(transacoes)), transacoes, preco, data):
+           print(f'ID {i} | {item.capitalize()} | R$ {valor:2f} | Data: {dt}')
+    elif opcao=='2':
+        print('\n Cadastrar uma nova Transição')
+        novo_item=input('Digite o nome do gasto(ex: Luz)')
+        novo_preco=float(input('informe o preço'))
+        nova_data=input('digite a data(ex 13/06/26)')
+        
+        transacoes.append(novo_item)
+        preco.append(novo_preco)
+        data.append(nova_data)    
+        print(f'\nSucesso! '{novo_item}' Foi adicionado.')
